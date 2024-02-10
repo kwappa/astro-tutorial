eae02eef74a4125c84d60229aae577ada8160c31
Hiromu Shioya <kwappa.856@gmail.com>
2024-02-10 15:50:25 +0900

    :v: Configure vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,16 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+// Import /static for a static site
+import vercelStatic from '@astrojs/vercel/static';
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+  // Must be 'static' or 'hybrid'
+  output: 'static',
+  adapter: vercelStatic(),
+
 });
+
